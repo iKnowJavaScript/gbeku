1e5f128f8191c54b5a500529c460137159974907
Omolayo Victor <vomolayo@seamfix.com>
2019-10-25 19:02:03 +0100

    Add a sample route and controller

--- src/controllers/sample.ts
+++ src/controllers/sample.ts
@@ -1,5 +1,9 @@
-function sampleController() {
-  return { hello: 'Hello World' };
+const allUsers = [{ name: "victor", age: 25 }, { name: "john", age: 25 }];
+
+class sampleController {
+  async getUser(id: number) {
+    return await allUsers[id];
+  }
 }
 
-export default sampleController;
+export default new sampleController();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import sampleRoute from "./sample.route";
+
 const router = Router();
 
 /** GET /health-check - Check service health */
 router.get("/health-check", (_req, res) => res.send("OK"));
+
+//mount sample route
+router.use("/sample", sampleRoute);
 
 export default router;
