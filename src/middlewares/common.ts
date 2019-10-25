c7ac5393d82036ffa68c54d18225239ebcbabfe2
Omolayo Victor <vomolayo@seamfix.com>
2019-10-25 19:01:19 +0100

    Add swagger and common express middleware

--- src/middlewares/common.ts
+++ src/middlewares/common.ts
@@ -5,6 +5,11 @@ import compression from "compression";
 import logger from "morgan";
 import methodOverride from "method-override";
 import helmet from "helmet";
+import expressWinston from "express-winston";
+import { winstonInstance } from "../config/logger";
+require("../../env");
+
+const env = process.env.NODE_ENV;
 
 const handleCors = (router: Router) =>
   router.use(cors({ credentials: true, origin: true }));
@@ -29,6 +34,24 @@ const handleHelmet = (router: Router) => {
 
 const handleOveride = (router: Router) => {
   router.use(methodOverride());
+};
+
+const apiLogger = (router: Router) => {
+  // enable detailed API logging in dev env
+  //comment this code to reduce api logs
+  if (env === "development") {
+    router.use(logger("dev"));
+    expressWinston.responseWhitelist.push("body");
+    router.use(
+      expressWinston.logger({
+        winstonInstance,
+        meta: true, // optional: log meta data about request (defaults to true)
+        msg:
+          "HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms",
+      }),
+    );
+  }
+  return;
 };
 
 export default [
---
+++ src/routes/sample.route.ts
@@ -0,0 +1,26 @@
+import sampleController from "../controllers/sample";
+
+import { Router, Request, Response } from "express";
+const router = Router();
+
+router.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const user = await sampleController.getUser(Number(id));
+
+  if (!user) {
+    return res.status(400).send({
+      success: "false",
+      message: "user not available",
+      payload: null,
+    });
+  }
+
+  return res.status(201).send({
+    success: "true",
+    message: "user retrieved successfully",
+    payload: user,
+  });
+});
+
+export default router;
